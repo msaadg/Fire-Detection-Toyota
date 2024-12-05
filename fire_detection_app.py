0226fca8643b359a8104739359b2d833a7a47d68
muhd.anas70pe@gmail.com <96944434+AlphaAnas@users.noreply.github.com>
2024-12-05 08:53:51 +0500

    added red bounding box code - have to make better

--- fire_detection_app.py
+++ fire_detection_app.py
@@ -7,7 +7,7 @@ def main():
     model = YOLO('trained-models/best-luminous.pt')  # Update the path if necessary
 
     # Initialize video capture (0 for default camera)
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print("Error: Cannot access the camera.")
@@ -21,7 +21,7 @@ def main():
             break
 
         # Resize frame for consistency
-        frame = cv2.resize(frame, (1280, 960))
+        frame = cv2.resize(frame, (980, 800))
 
         # Process the frame with YOLO
         results = model.predict(source=frame, conf=0.25, save=False, show=False)
@@ -29,8 +29,12 @@ def main():
         # Convert YOLO results to OpenCV format
         processed_frame = results[0].plot()
 
+        #   # Resize processed frame to match the original frame size
+        processed_frame = cv2.resize(processed_frame, (980, 800))
+
         # Concatenate the original and processed frames side by side
         combined_frame = np.hstack((frame, processed_frame))
+
 
         # Display the resulting frame
         cv2.imshow('Unprocessed (Left) | Processed (Right)', combined_frame)
